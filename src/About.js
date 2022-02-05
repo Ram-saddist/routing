464988b90f2b59cd431b2fe9bfed7fe2434591d4
Ram-saddist <nagulavancharavisivaramteja@gmail.com>
2022-02-05 19:05:23 +0530

    pushing router library code

---
+++ src/About.js
@@ -0,0 +1,9 @@
+function About(){
+	return(
+		<div>
+			This is from About Component
+		</div>
+	)
+}
+
+export default About
---
+++ src/Contact.js
@@ -0,0 +1,9 @@
+function Contact(){
+	return(
+		<div>
+			This is from Contact Component
+		</div>
+	)
+}
+
+export default Contact
